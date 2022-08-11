f0afd63957b23881a5e887e22e2d2dafd5063d17
Derek <derekyoung_8@msn.com>
2022-08-11 13:42:18 -0400

    Create logic to add books to library array

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let myLibrary = [];
+const btnSubmitNewBook = document.getElementById("submit-new-book");
+const inputTitle = document.getElementById("title");
+const inputAuthor = document.getElementById("author");
+const inputPages = document.getElementById("pages");
+const inputRead = document.getElementById("read");
+
+let myLibrary = [
+    // { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 350, read: true },
+    // { title: "The Intelligent Investor", author: "Benjamin Graham", pages: 400, read: false }
+];
+
+btnSubmitNewBook.addEventListener('click', () => {
+    addBookToLibrary()
+});
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -9,5 +22,12 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
 
+    let newBook = new Book(
+        inputTitle.value,
+        inputAuthor.value,
+        inputPages.value,
+        inputRead.checked
+    )
+    myLibrary.push(newBook);
 }
 
